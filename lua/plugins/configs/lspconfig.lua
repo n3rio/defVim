4b43c40088abe7729358c12fa5ec74342df73143
Nerio <nrincon.mr@gmail.com>
2021-09-29 16:46:45 -0500

    remove virtual texts

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -82,10 +82,11 @@ lspSymbol("Hint", "")
 lspSymbol("Warning", "")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-   virtual_text = {
-      prefix = "",
-      spacing = 0,
-   },
+   -- virtual_text = {
+   --    prefix = "",
+   --    spacing = 0,
+   -- },
+   virtual_text = false,
    signs = true,
    underline = true,
    update_in_insert = false, -- update diagnostics insert mode
