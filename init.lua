c08446be5c031079a81ae56507fb1067d0a03024
nerio <nrincon.mr@gmail.com>
2021-11-14 16:10:11 -0500

    new config, familiar to me, NOT nvChad

---
+++ init.lua
@@ -0,0 +1,34 @@
+--[[
+
+  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+
+
+Neovim init file
+Version: 0.32.0 - 2021/11/08
+Maintainer: Brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-----------------------------------------------------------
+-- Import Lua modules
+-----------------------------------------------------------
+require('settings')
+require('keymaps')
+require('plugins/packer')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-autopairs')
+require('plugins/feline')
+require('plugins/vista')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/bufferline')
+require('plugins/telescope')
+require('plugins/kommentary')
